ab9ba0647e382770cd0339b22a35a7f248eb8b40
akshay-bhatt <akshaybhatt2412@gmail.com>
2017-08-09 06:12:42 -0700

    Add files via upload

--- Backend/services/sensors.js
+++ Backend/services/sensors.js
@@ -1,29 +1,21 @@
 var sensorSchema = require('./schema/sensorSchema');
 var subscribeSensorSchema = require('./schema/subscribeSensorSchema');
 
-
 exports.loginSensorAdmin = function (msg, callback) {
-
     var username = msg.username;
     var password = msg.password;
     console.log("into the loginSensorAdmin queue response");
-
     var json_responses;
-
         if (username == "admin" && password == "admin") {
-
             json_responses = {"status" : 200, "data": username};
-        }
-        else {
+        } else {
             json_responses = {"status" : 400};
         }
-        callback(null, json_responses);
-    }
+    callback(null, json_responses);
+}
 
 exports.addSensorData = function(msg, callback){
-
     console.log("here I am");
-
     var name = msg.name;
     var desc = msg.desc;
     var owner = msg.owner;
@@ -33,12 +25,8 @@ exports.addSensorData = function(msg, callback){
     var format = msg.format;
     var city = msg.city;
     var state = msg.state;
-
-
-
     console.log(name + " " + desc + " " + owner + " " + to + " " + from + " " + type + " " + format + " " + city + " " + state);
     var sensorData = new sensorSchema({
-
         name : name,
         desc : desc,
         owner : owner,
@@ -49,53 +37,34 @@ exports.addSensorData = function(msg, callback){
         city : city,
         state : state,
         status : false
-
     });
-
     sensorData.save(function(err) {
-    console.log("Inside the save function");
-    var json_responses;
-    if (err) {
-        console.log(err);
-        json_responses = { "status" : 400};
-    }
-
-    else {
-        console.log('New sensor added !');
-
-        json_responses = {"status" : 200};
-
-    }
-        callback(null, json_responses);
-});
-
+        console.log("Inside the save function");
+        var json_responses;
+        if (err) {
+            console.log(err);
+            json_responses = { "status" : 400};
+        } else {
+            console.log('New sensor added !');
+            json_responses = {"status" : 200};
+        }
+            callback(null, json_responses);
+    });
 }
 
-
-
 exports.listSensors = function(msg, callback) {
-
-
     sensorSchema.find({}, function (err, users) {
-
         console.log("inside");
         var json_responses;
         if (err) {
-
             json_responses = {"status": 400};
-
-        }
-
-        else {
+        } else {
             if (users) {
                 //console.log(users.name+" "+users.type+" "+users.location);
                 json_responses = {"status": 200, "data": users};
-
-            }
-            else {
+            } else {
                 console.log("No sensors found !");
                 json_responses = {"status": 300};
-
             }
         }
         callback(null, json_responses);
@@ -103,21 +72,18 @@ exports.listSensors = function(msg, callback) {
 }
 
 exports.activateSensor = function(msg, callback) {
-
     var name = msg.name;
     console.log("sensor name is "+name);
     sensorSchema.update({name: name}, {$set:{status : true}}, function (err, users) {
         var json_response;
         console.log("-------------updated----------");
         console.log(JSON.stringify(users));
-        if (err)
+        if (err){
             json_response = {"status": 400};
-        else {
+        } else {
             if (users){
                 json_response = {"status": 200, "data": users};
                 //subscribeSensorSchema.find({}, function (err, user) {
-
-
                 /*subscribeSensorSchema.insert({"status" : users.status,"state":users.state, "format" : users.format, "from": users.from, "to": users.to, "name" : users.name, "desc" : users.desc, "city":users.city, "owner" : users.owner, "type" : users.type }, function(err, user){
                     if(err){
                         console.log("error");
@@ -125,34 +91,29 @@ exports.activateSensor = function(msg, callback) {
                     else
                         console.log("saras");
                 });*/
-
-            }
-
-            else
+            } else {
                 json_response = {"status": 300};
+            }
         }
         callback(null, json_response);
     });
 }
 
 exports.deactivateSensor = function(msg, callback) {
-
     var name = msg.name;
     console.log("sensor name is for deactivate"+name);
     sensorSchema.update({name: name}, {$set:{status : false}}, function (err, users) {
         var json_response;
         console.log(JSON.stringify(users));
-        if (err)
+        if (err){
             json_response = {"status": 400};
-        else {
-            if (users)
+        } else {
+            if (users) {
                 json_response = {"status": 200, "data": users};
-            else
+            } else {
                 json_response = {"status": 300};
+            }
         }
         callback(null, json_response);
     });
 }
-
-
-
